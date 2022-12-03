144e6f3d4329bf8fc6a41e7e9fe445f9c4cecfd2
Andre <andreluisfranca98@gmail.com>
2022-12-03 09:27:05 +0000

    Transfer to MongoDB

--- backend/django/backend/settings.py
+++ backend/django/backend/settings.py
@@ -92,8 +92,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'CeDRI_UGV',
+        'CLIENT': {
+           'host': 'mongodb://localhost:27017',
+        }
     }
 }
 
